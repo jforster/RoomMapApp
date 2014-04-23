3760c134cdc61ccb7e7f382c3a2f25b09bdf9de6
jforster2011 <jforster2011@nmt.edu>
2014-04-23 16:32:53 -0600

    Database
    created the database tables

--- gen/com/iadf/roommapapp/R.java
+++ gen/com/iadf/roommapapp/R.java
@@ -18,38 +18,41 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int BottomButtons=0x7f080000;
-        public static final int RoomViewer=0x7f080001;
-        public static final int TopButtons=0x7f080002;
-        public static final int action_settings=0x7f080004;
-        public static final int container=0x7f080003;
+        public static final int BottomButtons=0x7f090002;
+        public static final int RoomViewer=0x7f090001;
+        public static final int TopButtons=0x7f090000;
+        public static final int action_settings=0x7f090004;
+        public static final int container=0x7f090003;
     }
     public static final class layout {
         public static final int fragment_room_viewer=0x7f030000;
         public static final int room_viewer=0x7f030001;
     }
     public static final class menu {
-        public static final int room_viewer=0x7f070000;
+        public static final int room_viewer=0x7f080000;
+    }
+    public static final class raw {
+        public static final int sql=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int back=0x7f050003;
-        public static final int create_furniture=0x7f050007;
-        public static final int create_room=0x7f050005;
-        public static final int delete_room=0x7f050006;
-        public static final int hello_world=0x7f050001;
-        public static final int load_room=0x7f050004;
-        public static final int lookup_furniture=0x7f050008;
-        public static final int modify_room=0x7f050009;
-        public static final int view_furniture=0x7f05000a;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int back=0x7f060003;
+        public static final int create_furniture=0x7f060007;
+        public static final int create_room=0x7f060005;
+        public static final int delete_room=0x7f060006;
+        public static final int hello_world=0x7f060001;
+        public static final int load_room=0x7f060004;
+        public static final int lookup_furniture=0x7f060008;
+        public static final int modify_room=0x7f060009;
+        public static final int view_furniture=0x7f06000a;
     }
     public static final class style {
         /** 
@@ -73,10 +76,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/iadf/roommapapp/RoomViewer.java
+++ src/com/iadf/roommapapp/RoomViewer.java
@@ -1,7 +1,6 @@
 package com.iadf.roommapapp;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,14 +8,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class RoomViewer extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.room_viewer);
+		
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
